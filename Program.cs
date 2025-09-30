39170a7051b2e5cd0d0818c3c004f02524eab633
Jared Haynes <jhaynes4@my.wctc.edu>
2025-09-30 14:10:34 -0500

    {1.12a} Remove Character

--- Program.cs
+++ Program.cs
@@ -49,8 +49,14 @@ do
     else if (choice == "3")
     {
         // Remove Mario Character
-    }
-    else if (string.IsNullOrEmpty(choice))
+        Console.WriteLine("Enter the Id of the character to remove:");
+        if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
+        {
+            logger.Info($"Character Id {Id} entered");
+        } else {
+            logger.Error("Invalid Id");
+        }
+    } else if (string.IsNullOrEmpty(choice))
     {
         break;
     }
